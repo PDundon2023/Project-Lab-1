db96994a39e1495670fea585c835ab4cb6836b8d
PDundon2023 <90492389+PDundon2023@users.noreply.github.com>
2021-11-08 13:42:18 -0600

    Add files via upload

--- IPS_sensor.v
+++ IPS_sensor.v
@@ -1,174 +1,214 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/05/2021 12:30:08 PM
-// Design Name: 
-// Module Name: IPS_sensor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module IPS_sensor(
-
-    input ips_r, ips_L, clk,obs_det,  
-    
-    output JC3,JC4,JC9,JC10, pwm1,pwm2
-    
+    input ips_r, ips_L, clk,obs_det,     
+    output RMF,RMB,LMF,LMB, LM_pwm,RM_pwm   
     );
     
     reg[22:0] counter_r;
     reg[22:0] counter_L;
     reg[22:0] pulsewidth_r;
-    reg[22:0] pulsewdth_L;
-    reg curr_pwm_r;
-    reg curr_pwm_L;
+    reg[22:0] pulsewidth_L;
+    reg RM_pwm_temp;
+    reg LM_pwm_temp;
     
-    reg[6:0] motor_temp;
-    reg JC3_temp, JC4_temp, JC9_temp, JC10_temp;
-    reg[6:0] state_temp;
-    
-    always@(posedge clk)
+    reg [6:0] motor_temp;
+    reg RMF_temp, RMB_temp, LMF_temp, LMB_temp;
+    reg [6:0] state_temp;
+    reg [6:0] obs_state;
+    reg state_two;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
+always@(posedge clk)
     begin
         if(counter_r == 1666667)
             counter_r <= 0;
         else
             counter_r <= counter_r +1;
         if(counter_r < pulsewidth_r)
-            curr_pwm_r  = 1;
+            RM_pwm_temp  = 1;
         else
-            curr_pwm_r = 0;  
+            RM_pwm_temp = 0;  
     end
     
-    always@(posedge clk)
+always@(posedge clk)
     begin
         if(counter_L == 1666667)
             counter_L <= 0;
         else
             counter_L <= counter_L +1;
-        if(counter_L < pulsewidth_r)
-            curr_pwm_L  = 1;
+        if(counter_L < pulsewidth_L)
+            LM_pwm_temp = 1;
         else
-            curr_pwm_L = 0;  
+            LM_pwm_temp = 0;  
     end
-    
-    always@(*)
-    begin
-       if(obs_det == 0)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+ 
+
+always@(*)
+begin
+    if(obs_det == 0)
+     begin
+        state_temp = 4'd1;
+        obs_state = 4'd0;
+     end
+
+    case(state_temp)
+    4'd0: // normal state
         begin
-            state_temp = 4'd3; //Stop
-        end
-       else if(ips_L == 1 && ips_r == 1)
-        begin
-            state_temp = 4'd0; //Forward
-        end
-       else if(ips_r == 1)
-        begin
-            state_temp = 4'd2; // Right Turn
-        end
-       else if(ips_L == 1)
-        begin
-            state_temp = 4'd1; // Left Turn
-        end
-       else
-        begin
-            state_temp = 4'd0; // Forward
-        end
-    end
-    
-    always@(*)
-        case(state_temp)
-            4'd0: // rover continues forwards - neither ips sensor detected anything
+           if(ips_L == 1 && ips_r == 1)
                 begin
-                    pulsewdth_L  = 833334;
+                    pulsewidth_L = 833334;
+                    pulsewidth_r = 833334;
+                    motor_temp = 4'd0;
+                end 
+          else if(ips_L == 1)
+                begin
+                    pulsewidth_L = 833334;
+                    pulsewidth_r = 833334;
+                    motor_temp = 4'd1;
+                end
+          else if(ips_r == 1)
+                begin
+                    pulsewidth_L = 833334;
+                    pulsewidth_r = 833334;
+                    motor_temp = 4'd2;
+                end
+          else
+                begin
+                    pulsewidth_L = 833334;
                     pulsewidth_r = 833334;
                     motor_temp = 4'd0;
                 end
-            4'd1: // rover turns left - left ips sensor detected something
-                begin
-                    pulsewdth_L  = 500000;
+        end
+   4'd1: // obstacle state
+        begin
+            case(obs_state)
+            4'd0:
+             begin
+                if(ips_r == 0)
+                 begin
+                    pulsewidth_L = 833334;
                     pulsewidth_r = 833334;
-                    motor_temp = 4'd1;      
-                end
-            4'd2: // rover turns right - right ips sensor detected something
-                begin
-                    pulsewdth_L  = 833334;
-                    pulsewidth_r = 500000;
-                    motor_temp = 4'd2;    
-                end
-           4'd3:
-                begin
-                    pulsewdth_L = 0;
-                    pulsewidth_r = 0;
-                    motor_temp = 4'd3;
-                end
-            default: // rover will continue forward
-                begin
-                    pulsewdth_L  = 833334;
+                    motor_temp = 4'd4;
+                 end
+                else
+                 begin 
+                    obs_state = 4'd1;
+                 end
+             end
+            4'd1:
+             begin
+                if(ips_r == 1)
+                 begin
+                    pulsewidth_L = 833334;
                     pulsewidth_r = 833334;
-                    motor_temp = 4'd0;   
+                    motor_temp = 4'd2;
+                 end
+                else
+                 begin
+                    obs_state = 4'd2;
+                 end
+             end
+            4'd2:
+             begin
+                state_temp = 4'd0;
+                obs_state = 4'd3;
+             end
+            default:
+             begin
+                state_temp = 4'd0;
+             end         
+            endcase          
+        end
+   default:
+        begin
+            if(ips_L == 1 && ips_r == 1)
+                begin
+                    pulsewidth_L = 833334;
+                    pulsewidth_r = 833334;
+                    motor_temp = 4'd0;
+                end 
+          else if(ips_L == 1)
+                begin
+                    pulsewidth_L = 833334;
+                    pulsewidth_r = 833334;
+                    motor_temp = 4'd1;
                 end
-        endcase
-     always@(*)
+          else if(ips_r == 1)
+                begin
+                    pulsewidth_L = 833334;
+                    pulsewidth_r = 833334;
+                    motor_temp = 4'd2;
+                end
+          else
+                begin
+                    pulsewidth_L = 833334;
+                    pulsewidth_r = 833334;
+                    motor_temp = 4'd0;
+                end       
+        end       
+    endcase
+end
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+always@(*)
      begin 
         case(motor_temp)
             4'd0: // forwards
                 begin
-                    JC3_temp  =  0;
-                    JC4_temp  =  1;
-                    JC9_temp  =  1;
-                    JC10_temp =  0;
+                    RMF_temp  =  1;
+                    RMB_temp  =  0;
+                    LMF_temp  =  1;
+                    LMB_temp  =  0;
                 end
             4'd1: // left turn
                 begin
-                    JC3_temp  =  1;
-                    JC4_temp  =  0;
-                    JC9_temp  =  1;
-                    JC10_temp =  0;
+                    RMF_temp  =  1;
+                    RMB_temp  =  0;
+                    LMF_temp  =  0;
+                    LMB_temp  =  1;
                 end
            4'd2: // right turn
                 begin
-                    JC3_temp  =  0;
-                    JC4_temp  =  1;
-                    JC9_temp  =  0;
-                    JC10_temp =  1;
+                    RMF_temp  =  0;
+                    RMB_temp  =  1;
+                    LMF_temp  =  1;
+                    LMB_temp  =  0;
                 end
-          4'd3:
+          4'd3: // Stop
                 begin
-                    JC3_temp  =  0;
-                    JC4_temp  =  0;
-                    JC9_temp  =  0;
-                    JC10_temp =  0;  
+                    RMF_temp  =  0;
+                    RMB_temp  =  0;
+                    LMF_temp  =  0;
+                    LMB_temp  =  0;
+                end
+          4'd4: // Backwards
+                begin
+                    RMF_temp  =  0;
+                    RMB_temp  =  1;
+                    LMF_temp  =  0;
+                    LMB_temp  =  1;
                 end
            default: // forwards
                 begin
-                    JC3_temp  =  0;
-                    JC4_temp  =  1;
-                    JC9_temp  =  1;
-                    JC10_temp =  0; 
+                    RMF_temp  =  1;
+                    RMB_temp  =  0;
+                    LMF_temp  =  1;
+                    LMB_temp  =  0;
                 end
         endcase
      end
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
     
+    assign RM_pwm = RM_pwm_temp; //JC2
+    assign LM_pwm = LM_pwm_temp; //JC8
     
-    assign pwm1 = curr_pwm_L;
-    assign pwm2 = curr_pwm_r;
-    
-    assign JC3 = JC3_temp;
-    assign JC4 = JC4_temp;
-    assign JC9 = JC9_temp;
-    assign JC10 = JC10_temp;
+    assign RMF = RMF_temp;
+    assign RMB = RMB_temp;
+    assign LMF = LMF_temp;
+    assign LMB = LMB_temp;
     
     
 endmodule
